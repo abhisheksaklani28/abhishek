9100af9874657b2c845b3b76cb9d6c7a1e1dfe92
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-07-10 16:19:10 +0530

    pushing code 1st time

---
+++ class/src/student/biodata.java
@@ -0,0 +1,19 @@
+package student;
+
+public class biodata 
+{
+	public void m1()
+	{
+		System.out.println("this is method 1");
+	}
+    public void m2()
+{
+	System.out.println("this is method 2");
+	}
+    public static void main(String[] args) 
+    {
+    	biodata obj=new biodata();
+    	obj.m1();
+		
+	}
+}
